fbacbdcf72568930eb0c050a4a20b99a72cb4b41
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-06-06 18:56:40 -0400

    version_2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
 
+  def initialize( discount  = 0 )
+    @total = 0
+    @discount = discount
+  end
+
+  # attr_accessor :total
+  def total=(total)
+    @total = total
+  end
+
+  def total
+    @total
+  end
+
+  def discount
+    @discount
+  end
+
+  def add_item(title, price)
+
+  end
+
+
+end
