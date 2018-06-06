c4193703c3c78b6c6091b226571264f92d4ff862
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-06-06 19:32:01 -0400

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 class CashRegister
 
   def initialize( discount  = 0 )
     @total = 0
     @discount = discount
+    @items = []
+    @last_trans_amount = 0
   end
 
   # attr_accessor :total
@@ -18,9 +22,29 @@ class CashRegister
     @discount
   end
 
-  def add_item(title, price)
-
+  def add_item(title, price, quantity = 1)
+    quantity.times {@items.push(title)}
+    @last_trans_amount = price * quantity
+    # why does it not work with the variable name total
+    @total += (price * quantity)
   end
 
+  def apply_discount
+    if @discount != 0
+      @total = (@total/100 )* (100 - @discount)
+      # binding.pry
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @total - @last_trans_amount
+  end
 
 end
